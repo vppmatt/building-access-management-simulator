30993fe17a18cb8c4f1a153cdca1fead1f4d19de
Matt Thornfield <matt@oyvey.org.uk>
2024-11-14 12:04:51 +0000

    adding cross origin

--- src/main/java/com/neueda/buildingaccessmanagementsimulator/control/AccessController.java
+++ src/main/java/com/neueda/buildingaccessmanagementsimulator/control/AccessController.java
@@ -18,6 +18,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/logs")
+@CrossOrigin
 public class AccessController {
 
 
--- src/main/java/com/neueda/buildingaccessmanagementsimulator/control/UserController.java
+++ src/main/java/com/neueda/buildingaccessmanagementsimulator/control/UserController.java
@@ -3,6 +3,7 @@ package com.neueda.buildingaccessmanagementsimulator.control;
 import com.neueda.buildingaccessmanagementsimulator.data.UserRepository;
 import com.neueda.buildingaccessmanagementsimulator.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
+@CrossOrigin
 public class UserController {
 
     @Autowired
